6bcc1d71efceeb44a9ea4e0a0c3509301e393b32
Nikita Tsyrlin <nstsyrlin@gmail.com>
2016-06-08 20:04:58 +0300

    Added support of different endings for final list item

--- dist/list-item-punctuation.js
+++ dist/list-item-punctuation.js
@@ -1,31 +1,50 @@
 'use strict';
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
-
 var visit = require('unist-util-visit');
 var toString = require('mdast-util-to-string');
 var end = require('mdast-util-position').end;
+var defaults = require('object.defaults');
 
 function endingCheck(ast, file, preferred, done) {
-  var endings = ['.'];
-  if ((typeof preferred === 'undefined' ? 'undefined' : _typeof(preferred)) === 'object' && preferred.endings) {
-    endings = preferred.endings;
-  }
+  var defaultSettings = {
+    // Basic settings:
+    endings: ['.'],
+    final_endings: [],
 
-  visit(ast, 'listItem', function (node) {
-    var item = node.children[0];
-    var text = toString(item).trim();
+    // Loose lists:
+    loose: {
+      endings: ['.'],
+      final_endings: []
+    }
+  };
+  var settings = preferred || {};
+  defaults(settings, defaultSettings);
 
-    var _end = end(item);
+  visit(ast, 'list', function (node) {
+    var _loop = function _loop(i) {
+      var item = node.children[i].children[0];
+      var text = toString(item).trim();
 
-    var line = _end.line;
-    var column = _end.column;
+      var _end = end(item);
 
+      var line = _end.line;
+      var column = _end.column;
 
-    if (!endings.find(function (x) {
-      return x === text.substring(text.length - x.length);
-    })) {
-      file.warn('List items have to end up with "' + endings.join(' ') + '"', { line: line, column: column });
+      var endings = i === node.children.length - 1 && settings.final_endings.length > 0 ? settings.final_endings : settings.endings;
+      if (!endings.find(function (x) {
+        if (x === '') {
+          return (/\w$/.test(text)
+          );
+        }
+
+        return x === text.substring(text.length - x.length);
+      })) {
+        file.warn('List items have to end up with "' + endings.join(' ') + '"', { line: line, column: column });
+      }
+    };
+
+    for (var i = 0; i < node.children.length; i++) {
+      _loop(i);
     }
   });
   done();
